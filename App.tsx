e1a6997d8fa0d7c86610a661f8113c9f912550b3
Lechuck Roh <member@lechuck2011.cafe24.com>
2019-11-11 22:21:30 +0900

    add BarcodeScanner component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BarcodeScannerExample from './BarcodeScannerExample';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>테스트</Text>
+      <BarcodeScannerExample></BarcodeScannerExample>
     </View>
   );
 }
@@ -14,6 +16,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    alignSelf: 'stretch',
     justifyContent: 'center',
   },
 });
